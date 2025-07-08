e4da178650a57dfc84f0598b3b0d8132c174394a
imagination12357 <imagination12357@gmail.com>
2025-07-08 09:23:49 +0900

    One File Merge

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import streamlit as st
-from crawling import crawl
-from ollama_request import summarize
+import requests
+from bs4 import BeautifulSoup
+
+def summarize(prompt):
+    return "yeet"
+
+def refine(soup):
+    ul = soup.select_one('#mw-content-text > div.mw-content-ltr.mw-parser-output > ul:nth-of-type(1)')
+    removal = ul.find_all('sup', class_='reference')
+    for element in removal:
+        element.decompose()
+    return ul.get_text()
+
+def crawl(month, day):
+
+    url=f'https://ko.wikipedia.org/wiki/{month}월_{day}일'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return refine(soup)
+    else:
+        raise ValueError
 
 if 'scrapped' not in st.session_state:
     st.session_state.scrapped = None
